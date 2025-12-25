2c44c0a3e5bee817ee816db04c9b60cc961c2a28
Thakor Siddhraj Anilkumar <siddhrajthakor2304@gmail.com>
2025-12-25 13:23:58 +0530

    Create MinStack.py

---
+++ Stack/MinStack.py
@@ -0,0 +1,68 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# Implement the MinStack class:
+
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+# Example 1:
+
+# Input
+# ["MinStack","push","push","push","getMin","pop","top","getMin"]
+# [[],[-2],[0],[-3],[],[],[],[]]
+
+# Output
+# [null,null,null,null,-3,null,0,-2]
+
+# Explanation
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin(); // return -3
+# minStack.pop();
+# minStack.top();    // return 0
+# minStack.getMin(); // return -2
+ 
+
+# Constraints:
+
+# -231 <= val <= 231 - 1
+# Methods pop, top and getMin operations will always be called on non-empty stacks.
+# At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+
+class MinStack:
+
+    def __init__(self):
+        self.s=[]
+        self.mins=[]
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        val=min(val,self.mins[-1] if self.mins else val) # getting min value at each stage and compare it with the top of minstack and check if its empty or not
+        self.mins.append(val)
+        
+    def pop(self) -> None:
+        self.s.pop()
+        self.mins.pop()
+
+    def top(self) -> int:
+        return self.s[-1]        
+
+    def getMin(self) -> int:
+        return self.mins[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
